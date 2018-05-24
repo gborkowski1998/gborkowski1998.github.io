033bdbe320bc021825bb2dd118250afcd989e3fe
gborkowski1998 <39242254+gborkowski1998@users.noreply.github.com>
2018-05-24 19:22:29 +0200

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -20,49 +20,6 @@
     }
 })();
 
-var timesClicked = 0; //Counts how many times images were clicked
-
-(function () {
-    //ZOOM IN IMAGE
-    var images = ['img/1.png','img/2.png','img/3.png','img/4.png']; //Id of images
-    var places = ['.left-top-content',
-    '.left-bottom-content',
-    '.right-top-content',
-    '.right-bottom-content'
-    ]; //Position of Id
-    var websites = ['https://github.com/gborkowski1998/sklep',
-    'https://github.com/gborkowski1998/portfolio2017',
-    'https://github.com/gborkowski1998/projektE14',
-    'https://github.com/gborkowski1998/amping'
-    ]; //Links to websites
-    var textArea = document.querySelector('.projects'); //Section with text
-    var imagePlace = document.querySelector('.image-place');
-    //Div after textArea where image will be inserted
-    for (var z = 0; z < images.length; z++) {
-        let image = document.getElementById(images[z]);
-        let place = document.querySelector(places[z]);
-        let link = image.getAttribute("data-website-link");
-        //Takes "data-website-link" from image
-        image.addEventListener('click', () => {
-            textArea.style.display = 'none'; //Hide textArea
-            imagePlace.appendChild(image); //Insert image into imagePlace
-            image.style.cursor = 'pointer'; //Image got pointer cursor
-            timesClicked++; //Add +1 timesClicked
-        if(timesClicked%2==0) { //After EVERY two clicks
-            window.open(websites[link], '_blank'); //Open website
-            imagePlace.removeChild(image); //Delete image from imagePlace
-            let new_image = document.createElement("img"); //Create new image
-            new_image.setAttribute('class', 'new_image'); //New image class is new_image
-            new_image.setAttribute('width', '100%'); //New image width is 100%
-            new_image.setAttribute('height', '100p%'); //New image height is 100%
-            textArea.style.display = 'block'; //textArea is visible
-            place.appendChild(new_image);
-            //Insert new image into main position of clicked image
-            new_image.src = image.id; //New image is same as old one
-        }});
-    }
-})();
-
 (function () {
     //SCROLL DOWN
     var arrowDown = document.querySelector('.arrowDown'); //arrowDown image
